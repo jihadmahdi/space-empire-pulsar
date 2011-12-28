2be8decba7f0581f16b42c7ab39478b574fb4866
p.escallier@laposte.net <p.escallier@laposte.net@3f6be534-6438-0410-bf70-8b500f028278>
2011-12-28 22:13:25 +0000

    ORM dev suspended. At first, let's implement DBGraph manually, then we'll get back to ORM dev with better idea on how to design it.

--- trunk/src/main/java/org/axan/sep/common/db/IAntiProbeMissile.java
+++
@@ -1,8 +0,0 @@
-package org.axan.sep.common.db;
-
-public interface IAntiProbeMissile extends IUnit
-{
-	public String getTargetOwner();
-	public String getTargetName();
-	public Integer getTargetTurn();
-}
